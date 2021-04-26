2ee2004bcd99fa0a7d83303d0e3faa2e4c99d905
Afzal-Patel <aspb2017@mymail.pomona.edu>
2021-04-26 12:36:23 -0700

    working on denormalized indexes and queries

--- sql.denormalized/05.sql
+++ sql.denormalized/05.sql
@@ -1,3 +1,21 @@
 /*
  * Calculates the hashtags that are commonly used for English tweets containing the word "coronavirus"
  */
+
+SELECT
+    '#' || stripped_tag AS tag,
+    count(*) AS count
+FROM (
+    SELECT DISTINCT
+    data->>'id',
+    jsonb_array_elements(
+	COALESCE(data->'entities'->'hashtags','[]') ||
+	COALESCE(data->'extended_tweet'->'entities'->'hashtags','[]'))->>'text' AS stripped_tag
+    FROM tweets_jsonb
+    WHERE (to_tsvector('english', (data->>'text')) @@ to_tsquery('english','coronavirus') OR
+    to_tsvector('english', (data->'extended_tweet'->>'full_text')) @@ to_tsquery('english','coronavirus'))
+    AND data->>'lang' = 'en'
+) result
+GROUP BY stripped_tag
+ORDER BY count DESC, stripped_tag
+LIMIT 1000;
